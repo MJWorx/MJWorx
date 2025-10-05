46c22bcd4af68a468d5f4a12c70eada6e35d511e
Michael <lekalakala77@gmail.com>
2025-10-05 08:33:17 +0200

    Add files via upload

---
+++ SQL/bike_store_rational_data_part_2.sql
@@ -0,0 +1,32 @@
+USE Bike_Store_Rational_Data;
+
+
+SELECT * FROM bike_brands;
+
+SELECT * FROM bike_categories;
+
+SELECT * FROM bike_customers_1;
+
+SELECT * FROM bike_customers_2;
+
+SELECT * FROM bike_order_items_1;
+
+SELECT * FROM bike_order_items_2;
+
+SELECT * FROM bike_order_items_3;
+
+SELECT * FROM bike_order_items_4;
+
+SELECT * FROM bike_order_items_5;
+
+SELECT * FROM bike_orders_1;
+
+SELECT * FROM bike_orders_2;
+
+SELECT * FROM bike_products;
+
+SELECT * FROM bike_store;
+
+SELECT * FROM bike_stocks;
+
+SELECT * FROM bike_staff;
